dfa27ae7188205c7fc42b22302ad8eb2d09aa609
Aldwin Samson <aldwinsamsonn@gmail.com>
2026-02-09 14:50:04 +0800

    init

--- resources/js/pages/USHER/StockInU.tsx
+++ resources/js/pages/USHER/StockInU.tsx
@@ -228,7 +228,7 @@ const AddStockInModal: React.FC<{
     setErrors({});
     onClose();
   };
-
+  
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4">
       <div className="bg-white dark:bg-gray-800 rounded-2xl shadow-2xl w-full max-w-md max-h-[95vh] overflow-y-auto">
